c176d1d342df69e9d529c6b949cdc03fdef05ae9
Charlie Lee <rleecharlie@gmail.com>
2019-07-31 19:15:24 -0700

    to run in windows

---
+++ setup.cmd
@@ -0,0 +1,9 @@
+
+conda create --name ptorch
+activate ptorch
+
+#UNTESTED
+#conda install pytorch cuda91 -c pytorch 
+#pip3 install torchvision
+
+python test.py --saved_path test_models
---
+++ train.cmd
@@ -0,0 +1,4 @@
+call activate ptorch
+
+cd C:\tensorflow1\Super-mario-bros-A3C-pytorch
+python trainwindows.py
